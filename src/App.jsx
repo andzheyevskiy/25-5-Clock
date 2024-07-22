0ef56664c0ba129b83a5fa778572b3c4f94876bb
andzheyevskiy <Igor_lifeking@hotmail.com>
2024-07-22 21:28:08 +0200

    Implemented cronometers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Cronometro from "./assets/cronometro";
 
 function App() {
-  
+  const [breakNumber, setBreakNumber] = useState(5)
+  const [sessionNumber, setSessionNumber] = useState(25)
 
   return (
     <>
-     
+      <h1>25 + 5 Clock</h1>
+      <section className='cronometro-wrapper'>
+        <Cronometro
+          tipo="break"
+          currentMinutes={breakNumber}
+          changeMinutes={setBreakNumber}
+        />
+        <Cronometro
+          tipo="session"
+          currentMinutes={sessionNumber}
+          changeMinutes={setSessionNumber}
+        />
+      </section>
     </>
   )
 }
