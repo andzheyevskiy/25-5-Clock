81aea158c59c59c1caba1d771494e76e09eb5468
andzheyevskiy <Igor_lifeking@hotmail.com>
2024-07-23 20:02:27 +0200

    Refactoring + font awesome icons included

--- src/assets/contador.jsx
+++ src/assets/contador.jsx
@@ -1,27 +1,41 @@
 import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types';
+import "./contador.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faPause, faRepeat } from "@fortawesome/free-solid-svg-icons"
 
 
 function Contador(props) {
 
     const [breakNumber, setBreakNumber] = useState(props.breakNumber)
     const [sessionNumber, setSessionNumber] = useState(props.sessionNumber)
+    const [currentNumber,setCurrentNumber] = useState()
 
     const displaySeconds = String(props.seconds)[1] ? props.seconds : `0${props.seconds}`
 
+    function toggleBreak() {
+        if (props.isBreak) {
+            setBreakNumber(props.breakNumber)
+        }
+        else {
+            setSessionNumber(props.sessionNumber)
+        }
+        props.setIsBreak(e => !e)
+    }
+
     function countdown() {
-        if (props.seconds == 0) {
-            props.setSeconds(59)
-            if (props.isBreak && breakNumber > 0) {
+        if (currentNumber == 0 && props.seconds == 0) {
+            toggleBreak()
+        }
+        else if (props.seconds == 0) {
+            if (props.isBreak) {
                 setBreakNumber(e => e - 1)
-            } else if (!props.isBreak && sessionNumber > 0) {
-                setSessionNumber(e => e - 1)
             } else {
-                setBreakNumber(props.breakNumber - 1)
-                setSessionNumber(props.sessionNumber - 1)
-                props.setIsBreak(e => !e)
+                setSessionNumber(e => e - 1)
             }
-        } else {
+            props.setSeconds(59)
+        }
+        else {
             props.setSeconds(e => e - 1)
         }
     }
@@ -40,12 +54,16 @@ function Contador(props) {
             interval = setInterval(() => countdown(), 1000)
         }
         return () => clearInterval(interval)
-    }, [props.isStarted, props.seconds])
+    }, [currentNumber , props.isStarted, props.seconds])
 
     useEffect(() => {
         setBreakNumber(props.breakNumber)
         setSessionNumber(props.sessionNumber)
     }, [props.breakNumber, props.sessionNumber])
+
+    useEffect(()=>{
+        setCurrentNumber(props.isBreak?breakNumber:sessionNumber)
+    },[props.isBreak,breakNumber,sessionNumber])
 
     return (
         <>
@@ -57,9 +75,9 @@ function Contador(props) {
                     {displaySeconds}
                 </div>
                 <div className='button-wrapper'>
-                    <button id='start_stop' onClick={pauseResume}>Play</button>
-                    <button id='pause' onClick={pause}>Pause</button>
-                    <button id='reset' onClick={props.reset}>Reset</button>
+                    <button id='start_stop' onClick={pauseResume}><FontAwesomeIcon icon={faPlay} /></button>
+                    <button id='pause' onClick={pause}> <FontAwesomeIcon icon={faPause} /> </button>
+                    <button id='reset' onClick={props.reset}><FontAwesomeIcon icon={faRepeat} /></button>
                 </div>
             </section>
         </>
